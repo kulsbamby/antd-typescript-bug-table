d1b6dd2066a83661ee89b31b40a0d2380b2746fb
Kuls Bamby <kuls29071994@gmail.com>
2022-03-31 09:03:07 +0700

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import Sales from "./sales";
+
+ReactDOM.render(<Sales />, document.getElementById("root"));
